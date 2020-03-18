9219dc53540c00b19214da6627232df7148d2029
Zack Sarver <zsarver@gmail.com>
2020-03-17 23:31:35 -0400

    Adding search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from "styled-components"
-import {Header} from "./components"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import { Header, Search } from "./components"
 import background from './resources/1.jpeg';
 import './App.css';
 
@@ -12,14 +18,31 @@ background-repeat: no-repeat;
 background-size: cover;
 `
 
-function App({className}) {
+function App({ className }) {
   return (
-    <AppWrapper className={`${className} App`}>
-      <Header className={className} />
-      <div id="main"></div>
-    </AppWrapper>
+    <Router>
+      <AppWrapper className={`${className} App`}>
+        <Header className={className} />
+        <div id="main" className={className}>
+          <Switch>
+            <Route path="/" exact component={Search} />
+            <Route path="/forecast" component={() => <div></div>} />
+            <Route
+              component={() => {
+                return <Redirect to="/" />;
+              }}
+            />
+          </Switch>
+        </div>
+      </AppWrapper>
+    </Router>
   );
 }
 
 export default styled(App)`
+#main{
+  height: calc(100vh - 70px);
+  display: flex;
+  justify-content: center;
+}
 `;
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,11 +1,102 @@
-import React from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
+import { withRouter } from "react-router-dom";
+import styled from "styled-components";
+import {motion} from "framer-motion";
+import { usePosition } from 'use-position';
 
-const Search = () => {
+const SearchWrapper = styled.div`
+height: 12vh;
+width: 40vw;
+display:flex;
+flex-direction: column;
+background-color: white;
+border-radius: 10px;
+box-shadow: rgba(25, 17, 34, 0.55) 0px 3px 10px;
+padding: 1rem;
+margin-top: 15vh;
+text-align: left;
+`
+
+const Search = withRouter(({history, className}) => {
+    const [location, setLocation] = useState("");
+    const { latitude, longitude, timestamp, accuracy, error } = usePosition();
+    const [formError, setFormError] = useState("");
+    useEffect(() => {
+        let isSubscribed = true;
+    
+        const getCity = async () => {
+          try {
+            await fetch(
+              `https://geocode.xyz/${latitude},${longitude}?geoit=json`,
+              {
+                mode: "cors"
+              }
+            )
+              .then(response => response.json())
+              .then(data => {
+                if (isSubscribed){
+                    setLocation(data.city ? `${data.city}, ${data.state}` : `${latitude}, ${longitude}`);
+                } 
+              })
+          } catch (e) {
+            console.error(e);
+          }
+        };
+        if(timestamp)
+            getCity();
+
+        return () => (isSubscribed = false);
+      }, [latitude, longitude]);
+
+      const validateSearchForm = (e) => {
+        if(!e.target[0].value){
+            setFormError("Location is required");
+        } else {
+            history.push(`/forecast?q=${location}`);
+        }
+      }
     return (
-        <div>
-            
-        </div>
+        <SearchWrapper className={className}>
+            <h2 className={className}>Enter your location:</h2>
+            <form name="locationSearch" onSubmit={validateSearchForm} className={className} > 
+                <input name="location" type="text" placeholder="Enter your City, State" value={location} className={className}></input>
+                <button type="submit" className={`${className} btn-primary`}>Search</button>
+            </form>
+        </SearchWrapper>
     )
+});
+
+export default styled(Search)`
+h2{
+    margin-top:0px;
+}
+input{
+    height: 20px;
+    width: 100%
 }
 
-export default Search
+button{
+    margin-top: 5px;
+    align-self: center;
+    font-weight: 600;
+	display: inline-block;
+	font-weight: 400;
+	color: white;
+	text-align: center;
+	vertical-align: middle;
+	user-select: none;
+	background-color: transparent;
+	border: 1px solid transparent;
+	padding: .375rem .75rem;
+	font-size: 1rem;
+	line-height: 1.5;
+	border-radius: .25rem;
+}
+.btn-primary{
+    background-color: rgb(153, 31, 61);
+    border-style: solid;
+    border-color: rgb(102, 10, 33);
+    border-radius: 2px;
+}
+}
+`
