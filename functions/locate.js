480ae2f75b27da2e482ba6076a589ea2694c1b52
Zack Sarver <zsarver@gmail.com>
2020-03-21 22:14:04 -0400

    Editing locate

--- functions/locate.js
+++ functions/locate.js
@@ -1,27 +1,26 @@
-const fetch = require("node-fetch");
+const fetch = require("node-fetch").default;
 
-exports.handler = async function(event, context, callback) {
+exports.handler = async function (event, context, callback) {
   try {
-    await fetch(
-      `https://geocode.xyz/${event.queryStringParameters.lat},${event.queryStringParameters.long}?geoit=json`,
-      {
-        mode: "cors"
-      }
+    const location = await fetch(
+      `https://geocode.xyz/${event.queryStringParameters.lat},${event.queryStringParameters.long}?geoit=json`
     )
       .then(response => response.json())
       .then(data => {
-        callback(null, {
+        //console.log(data);
+        return ({
           statusCode: 200,
           body: data.city
             ? `${data.city}, ${data.state} ${data.prov}`
             : `${event.queryStringParameters.lat}, ${event.queryStringParameters.long}`
-        }).catch(e => {
-          console.error(e);
-          callback(new Error("Something went wrong"));
-        });
+        })
+      }).catch(e => {
+        console.log(e);
+        callback(new Error("Something went wrong"));
       });
+      callback(null, location)
   } catch (e) {
-    console.error(e);
+    console.log(e);
     callback(new Error("Something went wrong"));
   }
 };
